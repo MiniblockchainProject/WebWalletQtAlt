4dc4b1251f7f6b7fdfeccfb50a32d1e07b0cf2bd
MiniblockchainProject <MiniblockchainProject@users.noreply.github.com>
2014-09-04 05:46:21 +1000

    fixed bug stopping address archiving/reactivation

--- inc/pages/jobs/addtools.inc.php
+++ inc/pages/jobs/addtools.inc.php
@@ -22,18 +22,15 @@ if (isset($_GET['newadd'])) {
 } elseif (isset($_POST['moveadd'])) {
 
   list($move_mode, $address) = explode(':', $_POST['moveadd']);
-  
+  $acc_to = ($move_mode == 1) ? $_SESSION['account_id'] : $_SESSION['account_id'].'-archive';
+  $acc_fm = ($move_mode == 2) ? $_SESSION['account_id'] : $_SESSION['account_id'].'-archive';
   $address = preg_replace("/[^a-z0-9]/i", '', $address);
-  $addresses = $_SESSION[$rpc_client]->getaddressesbyaccount($_SESSION['account_id']);
+  $addresses = $_SESSION[$rpc_client]->getaddressesbyaccount($acc_fm);
   rpc_error_check();
-
+  
   if (!in_array($address, $addresses)) {
     die('error: private key for that address is unknown');
   }
-
-  $acc_to = ($move_mode == 1) ? $_SESSION['account_id'] : $_SESSION['account_id'].'-archive';
-  $acc_fm = ($move_mode == 2) ? $_SESSION['account_id'] : $_SESSION['account_id'].'-archive';
-  $addresses = $_SESSION[$rpc_client]->getaddressesbyaccount($acc_fm);
   
   if (count($addresses) > 1) {
     $_SESSION[$rpc_client]->setaccount($address, $acc_to, 1);
