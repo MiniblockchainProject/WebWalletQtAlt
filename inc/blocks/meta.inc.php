81fa6759c3ac067ac78998ece52f35419df7dfd4
MiniblockchainProject <MiniblockchainProject@users.noreply.github.com>
2015-01-21 15:09:40 +1100

    Removed gen_validatorv4 script

--- inc/blocks/meta.inc.php
+++ inc/blocks/meta.inc.php
@@ -26,7 +26,6 @@
 	<script src="js/html5shiv.min.js"></script>
 	<![endif]-->
 	<script src="./js/modernizr-2.6.2.min.js"></script>
-	<script src="./js/gen_validatorv4.js"></script>
 	<script src="./js/jquery-1.11.1.min.js"></script>
 	<script src="./js/jquery.form.min.js"></script>
 	<script src="./js/jquery.qrcode.min.js"></script>
